a64141cb2956b2bb49ec3799bc0640d848eee094
giansollie940 <giansollie940@gmail.com>
2026-08-22 23:26:27 +0700

    Add files via upload

--- features/account/password-policy.js
+++ features/account/password-policy.js
@@ -1,11 +1,1 @@
-export function validateStudentPassword(password) {
-  const value = String(password ?? "");
-  const hasMinLength = value.length >= 8;
-  const hasLetterAndNumber = /\p{L}/u.test(value) && /\d/u.test(value);
-
-  return {
-    valid: hasMinLength && hasLetterAndNumber,
-    hasMinLength,
-    hasLetterAndNumber
-  };
-}
+export function validateStudentPassword(password){const value=String(password||'');return {hasMinLength:value.length>=8,hasLetterAndNumber:/\p{L}/u.test(value)&&/\d/u.test(value),valid:value.length>=8&&/\p{L}/u.test(value)&&/\d/u.test(value)};}
