f4a4ce513df9df54fb59d4a334a0d6d4433f2850
giansollie940 <giansollie940@gmail.com>
2026-08-22 23:32:45 +0700

    Delete features/registration/registration-domain.js

--- features/registration/registration-domain.js
+++
@@ -1,9 +0,0 @@
-const COUNTED_STATUSES=new Set(["submitted","needs_revision","approved"]);
-
-export function isCountedRegistration(registration){
-  return Boolean(
-    registration
-    && !registration.isDeleted
-    && COUNTED_STATUSES.has(registration.status)
-  );
-}
